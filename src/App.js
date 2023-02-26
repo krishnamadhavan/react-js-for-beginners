6ea7cade70888109d7533cd50667e11d8d9f4d6c
Krishna Madhavan <krishnamadhavan07@gmail.com>
2023-02-26 20:25:56 +0530

    feat: add react components & props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World
+      <User name="Krishna Madhavan" designation="Senior Full Stack Developer" experience={3} />
     </div>
   );
+}
+
+
+const User = (props) => {
+  return (
+    <div>
+      <p><b>Name:</b> {props.name}</p>
+      <p><b>Designation:</b> {props.designation}</p>
+      <p><b>Years of Experience:</b> {props.experience}</p>
+    </div>
+  )
 }
 
 export default App;
